f73c76fea647ca1a4d9bde16af9cd3102d3f2a6c
Larik <illarionpecheritsa@yandex.ru>
2022-03-10 18:31:41 +0300

    верстка логина почти завершена

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './Form.css';
 
 export default function Form(props) {
@@ -10,6 +11,7 @@ export default function Form(props) {
                 <input className='form__input' />
                 <span className='form__error'>aaaaaaaa</span>
             <button type="submit" className='form__submit-button'>{props.buttonText}</button>
+            <p className='form__question'>Ещё не зарегистрированы? <Link to={'/'} className='reg__link'>Регистрация</Link></p>
         </form>
     );
 }
